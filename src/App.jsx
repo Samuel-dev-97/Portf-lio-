674b03ce91a1ffbe41020e9b72b0e474bbbc992b
Samuel-dev-97 <samuelpssouza@gmail.com>
2026-06-25 13:46:38 -0300

    Configuração inicial do portfólio e adição do logo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import skullLogo from './assets/skull.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+ return (
+   <>
+   <BrowserRouter>
+
+   </BrowserRouter>
+    
+   </>
+ )
+}
+
+
+export default App
